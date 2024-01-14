d6a4b6bb486a28654f79484c458e17f9a5d5ff92
arora-sumit09 <sumitarora1999@gmail.com>
2024-01-14 11:35:51 +0530

    added the ejs view engines and css

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
+app.set("view engine", "ejs");
 app.listen(3000);
 
-app.get('/' , (req,res)=>{
-    res.sendFile('./views/index.html' , {root : __dirname});
+app.get("/", (req, res) => {
+    const blogs = [
+        {title : "The Tawa Chicken" , snippet :"Its quite heavy and mix of khade masale"},
+        {title : "The Egg Curry" , snippet :"Its quite amazing combination of the egg and masala"},
+        {title : "Pizza" , snippet :"One of my favorite things to eat "}
+    ];
+  res.render("index", { title: "|Home" ,blogs });
 });
-app.get('/about' , (req,res)=>{
-    res.sendFile('./views/about.html' , {root : __dirname});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "| About" });
 });
-app.get('/about-me' , (req,res)=>{
-    res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "| Create" });
 });
 
 // 404 page
-app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html' , {root : __dirname});
+app.use((req, res) => {
+  res.status(404).render("404", { title: "| 404" });
 });
-
