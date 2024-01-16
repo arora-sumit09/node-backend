40fe5834e4f49b31b9c60c7a155319e58cf2ad52
arora-sumit09 <sumitarora1999@gmail.com>
2024-01-16 12:55:33 +0530

    addedd the middleware morgan and used static and created a public folder

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express");
 
 const app = express();
+const morgan = require("morgan");
 
 app.set("view engine", "ejs");
 app.listen(3000);
+app.use(morgan("dev"));
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    const blogs = [
-        {title : "The Tawa Chicken" , snippet :"Its quite heavy and mix of khade masale"},
-        {title : "The Egg Curry" , snippet :"Its quite amazing combination of the egg and masala"},
-        {title : "Pizza" , snippet :"One of my favorite things to eat "}
-    ];
-  res.render("index", { title: "|Home" ,blogs });
+  const blogs = [
+    {
+      title: "The Tawa Chicken",
+      snippet: "Its quite heavy and mix of khade masale",
+    },
+    {
+      title: "The Egg Curry",
+      snippet: "Its quite amazing combination of the egg and masala",
+    },
+    { title: "Pizza", snippet: "One of my favorite things to eat " },
+  ];
+  res.render("index", { title: "|Home", blogs });
 });
 app.get("/about", (req, res) => {
   res.render("about", { title: "| About" });
 });
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "| Create" });
 });
